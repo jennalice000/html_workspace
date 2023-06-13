a869f6dabe35deac1b530bc5c766b125a8923aac
jennalice000 <jennalice000@gmail.com>
2023-06-13 11:41:12 +0900

    :memo: 날씨정보 받아옴
    날씨정보받아옴

--- Project/project3/script2.js
+++ Project/project3/script2.js
@@ -1,6 +1,6 @@
-const icon = document.querySelector('.icon');
+const icon = document.querySelector('#header .icon');
 const button = document.querySelector('.button');
-const content = document.getElementById('content');
+const header = document.getElementById('header');
 const div = document.createElement('div')
 
 //navigator.geolocation.getCurrentPosition()-> 현재위치에 대한 정보를 끌어올 수 있다. (버튼은 실행부일뿐)
@@ -13,23 +13,6 @@ const API_KEY = '75e0fa0f573bccc184d63daabf91e9ff';
 
 
 //API이용해서 위도, 경도를 인수로 넣어 날씨에 대해 원하는 값 구하기
-//fetch버전
-// const getWeather = (lat, lon) => {
-//     fetch(
-//       `http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${API_KEY}`
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         const iconImg = data.weather[0].icon
-//         icon.src=`./weather/icons/${iconImg}.png`
-//         const description = data.weather[0].description;
-//         div.textContent = description;
-//         content.appendChild(div)
-//       })
-//       .catch((error) => {
-//         alert(error);
-//       });
-//   };
 
 //axios버전 (내가 더 선호하기 때문에 이거 씀, 이유 : 간편하고 보기 편해서)
 const getWeather = () => {
@@ -43,7 +26,7 @@ const getWeather = () => {
         icon.style.height="50px"
         const description = data.weather[0].description;
         div.textContent = description;
-        content.appendChild(div);
+        header.appendChild(div);
       })
       .catch((error) => {
         alert(error);
