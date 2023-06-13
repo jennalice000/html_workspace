c68277edb1d7791bb8679ee612506c419ca3df99
jennalice000 <jennalice000@gmail.com>
2023-06-13 15:06:17 +0900

    :memo: 달력완성
    달력완성

--- Project/project3/script.js
+++ Project/project3/script.js
@@ -1,7 +1,7 @@
 const background = document.getElementById('background');
 const passwordWindow = document.getElementById('passwordWindow');
 const pwd = document.getElementById('pwd');
-let pwdBackGround=background.querySelector=('.passwordBackground')
+let pwdBackGround = background.querySelector = ('.passwordBackground')
 
 function popUp() {
   background.style.filter = ''
@@ -16,7 +16,7 @@ function popUp() {
 }
 
 function reLock() {
-  document.addEventListener('keydown', function(e) {
+  document.addEventListener('keydown', function (e) {
     if (e.ctrlKey && e.altKey && e.shiftKey) {
       background.style.filter = ''
       passwordWindow.classList.remove('hide');
@@ -27,6 +27,7 @@ function reLock() {
 }
 
 function passwordWindowInterval() {
+
   setInterval(popUp, 1000000);
 }
 
@@ -39,51 +40,54 @@ let fallingWords = [];
 
 //위에서 떨어지는 단어 생성
 function createWords() {
-    let newWord = document.createElement('div');
-    newWord.innerText = words[Math.floor(Math.random() * words.length)];
-    newWord.className = 'word';
-  
-    newWord.style.position = 'absolute';
-    newWord.style.left = getRandomByRange(800, 1000) + 'px'; 
-    newWord.style.top = '250px';
-    newWord.style.fontSize = 17+'px'
-  
-    wrapper.appendChild(newWord);
-    fallingWords.push(newWord);
-  }
-  
+  let newWord = document.createElement('div');
+  newWord.innerText = words[Math.floor(Math.random() * words.length)];
+  newWord.className = 'word';
+
+  newWord.style.position = 'absolute';
+  newWord.style.left = getRandomByRange(800, 1000) + 'px';
+  newWord.style.top = '250px';
+  newWord.style.fontSize = 17 + 'px'
+  newWord.style.height = 'auto';
+  newWord.style.width = 'auto';
+
+  wrapper.appendChild(newWord);
+  fallingWords.push(newWord);
+}
+
 
 //랜덤함수
-function getRandomByRange(min, max){
-    let result=min + parseInt(((max-min)+1)*Math.random());
-    return result;
+function getRandomByRange(min, max) {
+  let result = min + parseInt(((max - min) + 1) * Math.random());
+  return result;
 }
 
 //다시 업데이트되어 떨어지는 단어 생성
 function updateWords() {
-    for (let i = 0; i < fallingWords.length; i++) {
-      let word = fallingWords[i];
-      let top = parseInt(word.style.top) || 0;
-      top += 5;
-      word.style.top = top + 'px';
-  
-      if (top >= 600) {
-        word.parentNode.removeChild(word);
-        fallingWords.splice(i, 1);
-        i--;
-      }
+  for (let i = 0; i < fallingWords.length; i++) {
+    let word = fallingWords[i];
+    let top = parseInt(word.style.top) || 0;
+    top += 5;
+    word.style.top = top + 'px';
+
+    if (top >= 600) {
+      word.parentNode.removeChild(word);
+      fallingWords.splice(i, 1);
+      i--;
     }
   }
-  
 
-addEventListener('load',function(){
-    setInterval(createWords, 2000);
-    setInterval(updateWords, 30);
-    passwordWindowInterval();
-    reLock();
+}
+
+
+addEventListener('load', function () {
+  setInterval(createWords, 2000);
+  setInterval(updateWords, 30);
+  passwordWindowInterval();
+  reLock();
 })
 
-pwd.addEventListener('input', function() {
+pwd.addEventListener('input', function () {
   popUp();
   if (pwd.value === 'apple') {
     passwordWindow.classList.add('hide');
--- Project/project3/script3.js
+++ Project/project3/script3.js
@@ -1,16 +1,115 @@
 let title;
 let dayTitle;
 let content;
-let dayArray=["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+let dayArray = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+let dateArray = [];
+let nowDate;
+
+//1. 요일박스를 만들어 붙인다
+//2. 날짜박스를 만들어 붙인다
+//3. 날짜 정보를 갖다 붙인다.
+//1. 시작 날짜가 어느 요일부터 시작하는가
+//2. 마지막 날짜가 '몇 일'로 끝나는가
+
+//4. 버튼 및 키보드를 누르면 이전, 이후 달로 이동
 
 
+//요일박스만들기
+function createDayBox() {
+    for (let i = 0; i < dayArray.length; i++) {
+        new DayBox(dayTitle, 100, 60, i * 100, 0, dayArray[i]);
+    }
+}
 
-function init(){
+//날짜박스만들기
+function createDateBox() {
+    for (let a = 0; a < 6; a++) { //층
+        let arr = new Array(7);//빈 배열 생성
+        for (let i = 0; i < dayArray.length; i++) {
+            let dateBox = new DateBox(days, 100, 100, i * 100, a * 100, "");
+            arr[i] = dateBox;
+
+            dateBox.div.addEventListener("click", function () {
+                console.log(innerText + '다');
+            })
+        };
+        dateArray.push(arr);
+    }
+}
+
+//달력제목세팅하기
+function setTitle(){
+    let year = nowDate.getFullYear();
+    let month = nowDate.getMonth();
+    title.innerText=year+"년 "+(month+1)+"월";
+}
+
+//마지막 날짜 구하기
+function getLastDateOfMonth(){
+    let d = new Date(nowDate.getFullYear(), nowDate.getMonth()+1); //현재 기준 다음 달
+    d.setDate(0); //d가 다음달이므로 다음달의 이전달인 이 달의 마지막날짜가 됨
+    return d.getDate();
+}
+
+//날짜박스에 날짜넣기
+function printDateNum(){
+    let startDay=nowDate.getDay(); //오늘 요일구하기 (init에서 오늘을 1일로 세팅해둠)
+    let lastDay=getLastDateOfMonth();
+
+    let count=0;
+    let n=0;
+
+    for(let i=0; i<dateArray.length; i++){
+        for(let j=0; j<dateArray[i].length;j++){
+            if(count>=startDay && n<lastDay){
+                n++;
+                dateArray[i][j].setText(n);
+            }else{
+                dateArray[i][j].setText("");
+            }
+            count++;
+        }
+    }
+}
+
+
+//초기화
+function init() {
+    //html의 요소들을 js로 가져온다.
     title = document.getElementById('title')
     dayTitle = document.getElementById('day_title')
     content = document.getElementById('content')
+    days = document.getElementById('days')
+
+    nowDate= new Date();
+    nowDate.setDate(1);
+    setTitle();
+    printDateNum();
+
 }
 
-addEventListener('load', function(){
+//로드
+addEventListener('load', function () {
     init();
+    createDayBox();
+    createDateBox();
+    printDateNum()
+
+    this.addEventListener('keydown', function(e){
+        if(e.key === 'ArrowLeft'){
+            nowDate.setMonth(nowDate.getMonth()-1);
+            nowDate.setDate(1);
+            setTitle();
+            printDateNum();
+        }
+    })
+    
+    this.addEventListener('keydown', function(e){
+        if(e.key === 'ArrowRight'){
+            nowDate.setMonth(nowDate.getMonth()+1);
+            nowDate.setDate(1);
+            setTitle();
+            printDateNum();
+        }
+    })
 })
